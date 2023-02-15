daa66a7af087b386bed53137363b2cd5bd50d329
dnjeh <ehdnj.001@gmail.com>
2023-02-15 20:34:16 +0900

    hurry!

---
+++ 1491.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n, m, i, j, cnt=0, x, y;
+    scanf("%d %d", &n, &m);
+    int a[n][m];
+    for(i=0;i<n;i++) for(j=0;j<m;j++) a[i][j]=0;
+    for(x=y=0;cnt<n*m;cnt++) {
+        
+    }
+}
