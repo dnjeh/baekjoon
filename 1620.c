739590ae10ad02bf69747c7763fde7a9979c8c5f
dnjeh <ehdnj.001@gmail.com>
2023-03-28 21:36:35 +0900

    Today's fimmallly completed(c3) baekjoon's

--- 1620.c
+++ 1620.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+int a1[100000]; 
+char *pa[100000], a[100000][21];
 void chimerge(char *arr[], int irr[], int left, int middle, int right) {
     int i, j, k, _i, len1, len2, n3;
     int n1 = middle-left+1;
@@ -8,19 +10,8 @@ void chimerge(char *arr[], int irr[], int left, int middle, int right) {
     for(i=0;i<n1;i++) { L[i]=arr[left+i]; iL[i]=irr[left+i]; }
     for(j=0;j<n2;j++) { R[j]=arr[middle+1+j]; iR[j]=irr[middle+1+j]; }
     for(i=0,j=0,k=left;i<n1&&j<n2;k++) {
-        len1=strlen(L[i]); len2=strlen(R[j]); 
-        if(len1<len2) { arr[k]=L[i]; irr[k]=iL[i++]; }
-        else if(len2<len1) { arr[k]=R[j]; irr[k]=iR[j++]; }
-        else for(_i=0;_i<len1;_i++) {
-            if((*(L[i]+_i)<*(R[j]+_i))) {
-                arr[k]=L[i]; irr[k]=iL[i++];
-                break;
-            }
-            else if(*(L[i]+_i)>*(R[j]+_i)||_i+1==len1) {
-                arr[k]=R[j]; irr[k]=iR[j++];
-                break;
-            }
-        }
+        if(strcmp(L[i], R[j])<0) { arr[k]=L[i]; irr[k]=iL[i++]; }
+        else { arr[k]=R[j]; irr[k]=iR[j++]; }
     }
     for(;i<n1;i++,k++) { arr[k]=L[i]; irr[k]=iL[i]; }
     for(;j<n2;j++,k++) { arr[k]=R[j]; irr[k]=iR[j]; }
@@ -36,8 +27,8 @@ void chimergeSort(char *arr[], int irr[], int left, int right) {
 int main() {
     int n, m, i, ter, j, up, dwn, mid, tt;
     scanf("%d %d", &n, &m);
-    int a1[n]; for(i=0;i<n;i++) a1[i]=i;
-    char *pa[n], a[n][21], t[21];
+    for(i=0;i<n;i++) a1[i]=i;
+    char t[21];
     for(i=0;i<n;i++) {
         scanf("%s", a[i]);
         pa[i]=a[i];
@@ -51,15 +42,7 @@ int main() {
         }
         else {
             for(up=n-1, dwn=0, mid=(dwn+up)/2;dwn<=up;mid=(dwn+up)/2) {
-                int len1=strlen(t), len2=strlen(pa[mid]); 
-                if(len1<len2) ter=-1;
-                else if(len2<len1) ter=1;
-                else for(int _i=0;_i<len1;_i++) {
-                    if(_i+1==len1) ter=0;
-                    else if((*(t +_i)<*(pa[mid]+_i))) { ter=-1; break; }
-                    else if(*(t+_i)>*(pa[mid]+_i)) { ter=1; break;}
-                }
-                if(!ter) { printf("%d\n", a1[mid]+1); break; }
+                if(!(ter=strcmp(t, pa[mid]))) { printf("%d\n", a1[mid]+1); break; }
                 else if(ter<0) up=mid-1;
                 else dwn=mid+1;
             }
