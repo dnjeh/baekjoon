30a7e0dedefac836b1e631cdce50c15109f75c64
dnjeh <ehdnj.001@gmail.com>
2023-05-04 23:19:40 +0900

    Today's ffiimmmmally-completed(b2) baekjoon's

--- 1252.c
+++ 1252.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-    int i, j, t1;
-    char t[81]={'\0',}, a[2][81]={'\0',}, fin[82]={'\0',};
+    int sum[90]={0, };
+    int i, j, b[2][2]={{-1, }, {-1, } }, i1, j1, f=0, zf=0;
+    char a[2][90];
     for(i=0;i<2;i++) {
-        scanf("%s", t);
-        for(j=strlen(t)-1;j>=0;j--) {
-            a[i][strlen(t)-1-j]=t[j];
+        scanf("%s", a[i]);
+        for(j=0;j<90&&a[i][j];j++) {
+            if(b[i][0]==-1&&a[i][j]=='1') b[i][0]=j;
+        }
+        b[i][1]=j-1;
+        if(b[i][0]<0) {
+            b[i][0]=0;
+            b[i][1]=0;
+            zf++;
         }
     }
-    for(i=0;a[0][i]!='\0'||a[1][i]!='\0';i++) {
-        if(a[0][i]=='1'&&a[1][i]=='1') {
-            fin[i]='2';
+    if(zf>1) {
+        printf("0");
+        return 0;
+    }
+    for(i1=b[0][1], j1=b[1][1], i=0;i<90&&i1>=b[0][0]||j1>=b[1][0];i++) {
+        if(i1>=0&&i1<90&&j1>=0&&j1<90&&a[0][i1]=='1'&&a[1][j1]=='1') {
+            sum[i]=2;
         }
-        else if(a[0][i]=='1'||a[1][i]=='1') {
-            fin[i]='1';
+        else if((i1>=0&&i1<90&&a[0][i1]=='1')||(j1>=0&&j1<90&&a[1][j1]=='1')) {
+            sum[i]=1;
         }
-        else {
-            fin[i]='0';
+        else sum[i]=0;
+        i1--; j1--;
+        if(i1<b[0][0]) i1=-1;
+        if(j1<b[1][0]) j1=-1;
+    }
+    for(j=0;j<90&&j<i;j++) {
+        if(sum[j]>1) {
+            sum[j]-=2;
+            sum[j+1]++;
         }
     }
-    for(i=0;fin[i]!='\0';i++) {
-        if(fin[i]=='3') {
-            (fin[i+1])?(fin[i+1]++):(fin[i+1]='1');
-            fin[i]='1';
-        }
-        else if(fin[i]=='2') {
-            (fin[i+1])?(fin[i+1]++):(fin[i+1]='1');
-            fin[i]='0';
-        }
-    }
-    for(strlen(fin)-1;i>=0;i--) {
-        printf("%c", fin[i]);
+    if(sum[i]) printf("1"); 
+    for(j=i-1;j<90&&j>=0;j--) {
+        printf("%d", sum[j]);
     }
 }
