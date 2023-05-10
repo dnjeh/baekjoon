6df9d3187080b994c68d15244ccb5c541ba00813
dnjeh(Notebook) <ehdnj.001@gmail.com>
2023-05-11 07:26:04 +0900

    Today's fimally(..)-completed-baekjoon's(s1)

--- 2915.c
+++ 2915.c
@@ -17,22 +17,32 @@ void tolom(int _t) {
     ret[ind]=0;
 }
 void readcnt(char _t[20], int _it[5]) {
+    int ret=0;
     for(int i=0;_t[i];i++) {
+        ret=-1;
         switch(_t[i]) {
-        case 'I':
-            break;
-        case 'V':
-        case 'X':
-        case 'L':
-        case 'C':
+        case 'C': ret++; 
+        case 'L': ret++;
+        case 'X': ret++;
+        case 'V': ret++;
+        case 'I': ret++;
         }
+        _it[ret]++;
     }
 } 
 int main() {
-    int cnt[5];
-    char t[20], tt[20];
+    int cnt[5]={0, }, pcnt[5], j;
+    char t[20]={'\0', };
     scanf("%s", t);
+    readcnt(t, cnt);
     for(int i=1;i<=99;i++) {
-
+        tolom(i);
+        for(j=0;j<5;j++) pcnt[j]=0;
+        readcnt(ret, pcnt);
+        for(j=0;j<5;j++) if(cnt[j]!=pcnt[j]) break;
+        if(j>4) {
+            printf("%s", ret);
+            return 0;
+        }
     }
 }
