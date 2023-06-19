7d78e0bf215c7894e165bcea7540844559a73462
dnjeh <ehdnj.001@gmail.com>
2023-06-19 20:02:10 +0900

    Twoday's uncompleted(c4) baekjoon's

--- _15663.c
+++ _15663.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-int t[10001], a[8], b[8], n, m, ind;
+int t[10001], a[8], b[8], n, m, ind, tocnt;
 void back(int cnt) {
-    int _t, _pre;
+    int _t, tcnt=0, mem[10001]={0, };
     if(cnt==m) {
         for(int i=0;i<m;i++) {
             printf("%d%c", b[i], i+1<m?' ':'\n');
         }
     }
-    else for(int i=0;i<ind;i++) {
-        if(a[i]&&(!i||_pre!=a[i])) {
+    else for(int i=0;tcnt+1<tocnt;i++) {
+        if(a[i]&&!mem[a[i]]) {
+            mem[a[i]]=1;
+            tcnt++;
             b[cnt]=_t=a[i];
             a[i]=0;
             back(cnt+1);
             a[i]=_t;
-            _pre=a[i];
         }
     }
 }
@@ -24,6 +25,13 @@ int main() {
         scanf("%d", &it);	
         t[it]++;
     } 
-    for(i=1;i<=10000;i++) if(t[i]) { a[ind++]=i; t[i]--; i--; }
+    for(i=1;i<=10000;i++) {
+        if(t[i]&&a[ind]!=i) tocnt++;
+        if(t[i]) { 
+            a[ind++]=i; 
+            t[i]--; 
+            i--;
+        }
+    }
     back(0);
 } 
