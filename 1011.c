56baa868967c38d7a9f6e396a7253b1d267a0aed
dnjeh <ehdnj.001@gmail.com>
2023-07-12 17:57:30 +0900

    Today's nojl-fimmally-completed(g5) baekjoon's

--- 1011.c
+++ 1011.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-unsigned int sum[50000];
-void sums() {
-    for(int i=0;i<50000;i++) {
-        sum[i]=(i+1)%2?(i+2)/2*(i+1):(i+1)/2*(i+2);
-    }
-}
-int main() {
-    unsigned int T, x, y, a, now, i, j, cnt;
-    scanf("%d", &T);
-    sums(); printf("%d", sum[10]);
-    for(i=0;i<T;i++) {
+#include <stdio.h> 
+long long int sum[50000]; 
+void sums() { 
+    for(long long int i=0;i<50000;i++) { 
+        sum[i]=(i+1)%2?(i+2)/2*(i+1):(i+1)/2*(i+2); 
+    } 
+} 
+int main() { 
+    long long int T, x, y, a, now, i, j, cnt; 
+    scanf("%lld", &T); 
+    sums(); 
+    for(i=0;i<T;i++) { 
         now=1; 
-        scanf("%d %d", &x, &y); a=y-x; 
+        scanf("%lld %lld", &x, &y); a=y-x; 
         if(a==1) {
-            printf("1");
+            printf("1\n");
             continue;
         }
         for(j=0;sum[j]*2<a;j++); cnt=j*2;
-        if(a-sum[j-1]*2<=j) cnt++;
+        if(a-sum[j-1]*2<=j+1) cnt++;
         else cnt+=2;
-        printf("%d\n", cnt); 
-    }
+        printf("%lld\n", cnt); 
+    } 
 }
