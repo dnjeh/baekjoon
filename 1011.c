8d2313bfacc2491659d61add0e6215f2f9a20d58
lilac <109151923+dnjeh@users.noreply.github.com>
2023-07-12 13:09:30 +0900

    un-completed baekjotwo's

--- 1011.c
+++ 1011.c
@@ -10,10 +10,15 @@ int main() {
     scanf("%d", &T);
     sums(); printf("%d", sum[10]);
     for(i=0;i<T;i++) {
-        cnt=0; now=1;
-        scanf("%d %d", &x, &y); a=y-x;
-        for(j=0;sum[j]*2<a;j++);
-        
-        printf("%d\n", cnt);
+        now=1; 
+        scanf("%d %d", &x, &y); a=y-x; 
+        if(a==1) {
+            printf("1");
+            continue;
+        }
+        for(j=0;sum[j]*2<a;j++); cnt=j*2;
+        if(a-sum[j-1]*2<=j) cnt++;
+        else cnt+=2;
+        printf("%d\n", cnt); 
     }
 }
