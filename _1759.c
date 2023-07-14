70e5488602c9a23bcce160f5e95dbaa109d00a85
lilac <109151923+dnjeh@users.noreply.github.com>
2023-07-14 09:54:18 +0900

    Today's completed-half-quater(g5) baekjoon's

--- _1759.c
+++ _1759.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-int l, c, now[15], alp[26];
+int l, c, now[15], alp[26], u[2];
 void bac(int min, int cnt) {
-    int i, j;
+    int i, j, t;
     if(cnt>=l) {
-        for(i=0;i<l;i++) printf("%c", now[i]+97);
-        printf("\n");
+        if(u[0]&&u[1]>1) {
+            for(i=0;i<l;i++) printf("%c", now[i]+97);
+            printf("\n");
+        }
     }
     else {
         for(i=min;i<26;i++) if(alp[i]) {
             now[cnt]=i;
+            if((t=i+97)=='a'||t=='e'||t=='i'||t=='o'||t=='u') u[0]++;
+            else u[1]++;
             alp[i]--;
             bac(i, cnt+1);
             alp[i]++;
+            if((t=i+97)=='a'||t=='e'||t=='i'||t=='o'||t=='u') u[0]--;
+            else u[1]--;
         }
     }
 }
