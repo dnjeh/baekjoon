8e0f20bd0a52c85e3c58bcd71e8d27ae3d37a7da
dnjeh <ehdnj.001@gmail.com>
2023-07-16 10:17:05 +0900

    Today's un-completed baekjoon's

---
+++ 1717.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int a[1000001], n, m;
+void set();
+
+int main() {
+    int i, j, m, t1, t2;
+    scanf("%d %d", &n, &m); set();   
+    for(i=0;i<m;i++) {
+        scanf("%d %d %d", &m, &t1, &t2);
+        if(!m) {
+
+        }
+        else {
+            
+        }
+    }
+}
+
+void set() {
+    int i;
+    for(i=0;i<=n;i++) a[i]=i;
+}
