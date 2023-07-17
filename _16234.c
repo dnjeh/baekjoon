97a1400d022934db5d489a7fc37590f6549bdfbc
dnjeh <ehdnj.001@gmail.com>
2023-07-17 10:15:54 +0900

    Today's still un-completed baekjoon's

--- _16234.c
+++ _16234.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
-int a[50][50], n, l, vis[50][50];
+int a[50][50], n, l, r, vis[50][50], q[3000][2];
 void set();
+int par(int y, int x, int dy, int dx) {
+    int t=a[y][x]-a[dy][dx]; 
+    if(t<0) t*=-1;
+    if(t>=l&&t<=r) return 1;
+    else return 0;
+}
+int bfs(int y, int x) {
+    int ind=1, i, _x, _y, sum=0, cal; q[0][0]=y; q[0][1]=x;
+    //vis[y][x]=1;
+    for(i=0;i<ind;i++) {
+        _y=q[i][0]; _x=q[i][1];
+        sum+=a[_y][_x];
+        if(_x-1>=0&&!vis[_y][_x-1]&&par(_y, _x, _y, _x-1)) {
+            q[ind][0]=_y;
+            q[ind++][1]=_x-1;
+            vis[_y][_x-1]=1;
+        }
+        if(_y-1>=0&&!vis[_y-1][_x]&&par(_y, _x, _y-1, _x)) {
+            q[ind][0]=_y-1;
+            q[ind++][1]=_x;
+            vis[_y-1][_x]=1;
+        }
+        if(_x+1<n&&!vis[_y][_x+1]&&par(_y, _x, _y, _x+1)) {
+            q[ind][0]=_y;
+            q[ind++][1]=_x+1;
+            vis[_y][_x+1]=1;
+        }
+        if(_y+1<n&&!vis[_y+1][_x]&&par(_y, _x, _y+1, _x)) {
+            q[ind][0]=_y+1;
+            q[ind++][1]=_x;
+            vis[_y+1][_x]=1;
+        }
+    } cal=sum/ind;
+    for(i=0;i<ind;i++) a[q[i][0]][q[i][1]]=cal;
+    if(ind!=1) return 0;
+    else return 1;
+}
 int main() {
     int i, j, d, f=0;
     scanf("%d %d %d", &n, &l, &r);
@@ -10,13 +47,21 @@ int main() {
         }
     }
     for(d=0;!f;d++) {
-        int ind=0;
         for(i=0;i<n;i++) {
             for(j=0;j<n;j++) {
-                if()
+                printf("%d ", a[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n\n");
+        set();
+        for(i=0;i<n;i++) for(j=0;j<n;j++) {
+            if(!vis[i][j]) {
+                f+=bfs(i, j);
             }
         }
     }
+    printf("%d", d-1);
 }
 
 void set() {
