df1bacc0452c0b172ceb30c15e63a50c151f9017
dnjeh <ehdnj.001@gmail.com>
2023-07-23 20:55:16 +0900

    Today's fimmmmaiiiiilly!!-last-g5-baekjoon'ss

--- 2447.c
+++ 2447.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
-int cnt, n;
-
-void sta(int )
-void star11(int _t, int f, int flo) {
-    int i, j, jj;
-    if(_t==1) {
-        cnt++;
-        printf("%c", f?'*':' ');
-        if(cnt>=n) { printf("\n"); cnt=0; }
-    }
+int n, l;
+void sta(int now, int f) {
+    int i, j;
+    if(now==1) printf("%c", f?'*':' ');
     else {
-        /*for(i=0;i<3;i++)*/ for(j=0;j<3;j++) {
-            for(jj=0;jj<3;jj++) {
-                if(j==flo) {
-                    star11(_t/3, f&&(j!=1||jj!=1)?1:0, i);
-                }
-            }
+        for(j=((l/(now/3))%3)*3;j<((l/(now/3))%3+1)*3;j++) {
+            sta(now/3, j!=4&&f?1:0);
         }
     }
 }
 int main() {
-    int i;
     scanf("%d", &n);
-    for(i=0;i<3;i++) {
-        star11(n, 1, i);
+    for(int i=0;i<n;i++) {
+        l=i;
+        sta(n, 1);
+        printf("\n");
     }
 }
