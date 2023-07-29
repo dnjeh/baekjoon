c54671f77b421f17d24098879ba889e076590957
dnjeh <ehdnj.001@gmail.com>
2023-07-29 23:33:51 +0900

    Today's (un)completed(c5) baekjoon's

--- _1027.c
+++ _1027.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int n, a[50];
+long long int n, a[50];
 int fun(int t) {
-    int i, j, cha, ii, ret=0;
+    long long int i, j, cha, ii, ret=0;
     for(i=0;i<n;i++) {
         if(i==t) continue;
         ii=t-i; cha=a[t]-a[i]; 
         for(j=i;ii<0?j>t:j<t;ii<0?j--:j++) {
-
+            if(a[i]<) break;
         }
-        if(j==cha) ret++;
+        if(j==t) ret++;
     }
     return ret;
 }
