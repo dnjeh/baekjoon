dd2e12c0293d04210b91c03c4b0b0e6fc5def493
dnjeh(Notebook) <ehdnj.001@gmail.com>
2023-08-08 13:02:16 +0900

    Today's deux-completed baekjoon's

--- _2638.c
+++ _2638.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-int a[100][100], vis[100][100], q[10100], let=0, r, c;
+int a[100][100], vis[100][100], q[10100][2];
+int ind, now, let=0, r, c;
+int chk(int y, int x) {
+    int ret=0;
+    if(x-1>=0&&!a[y][x-1]&&vis[y][x-1]) ret++;
+    if(x+1<c&&!a[y][x+1]&&vis[y][x+1]) ret++;
+    if(y-1>=0&&!a[y-1][x]&&vis[y-1][x]) ret++;
+    if(y+1<r&&!a[y+1][x]&&vis[y+1][x]) ret++;
+    if(ret>=2) return ret;
+    else return ret;
+}
+void qput(int y, int x) {
+    q[ind][0]=y;
+    q[ind++][1]=x;
+}   
+void bfs(int y, int x) {
+    qput(y, x);
+}
 int main() {
-    int i, j, cnt=0, ind, dind;
+    int i, j, cnt=0, dind;
     scanf("%d %d", &r, &c);
     for(i=0;i<r;i++) {
         for(j=0;j<c;j++) {
@@ -9,7 +26,16 @@ int main() {
             if(a[i][j]) let++;
         }
     }
-    for(cnt=0;let;cnt++) {
-
+    bfs(0, 0);
+    for(cnt=0;let>=0;cnt++) {
+        for(i=0;i<r;i++) {
+            for(j=0;j<c;j++) {
+                if(chk(i, j)) {
+                    a[i][j]=0;
+                    let--;
+                    qput(i, j);
+                }
+            }
+        }
     }
 }
