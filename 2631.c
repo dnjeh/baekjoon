365a70cd4454dc2a8a8e246c7e80e67df5c5ee9a
lilac <109151923+dnjeh@users.noreply.github.com>
2023-08-14 10:50:30 +0900

    Today's nnalllmuk-half-quater(g4) baekjoon's

---
+++ 2631.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int mem[200]={1, }, a[200], n;
+int dp(int t) {
+    int ret=0, i, tt;
+    if(mem[t]) ret=mem[t];
+    else {
+        for(i=t-1;i>=0;i--) {
+            tt=dp(i);
+            if(a[i]<a[t]&&(!ret||tt+1>ret)) {
+                ret=tt+1;
+            }
+        }
+        if(!ret) ret=1;
+        mem[t]=ret;
+    }
+    return ret;
+}
+int main() {
+    int i, max=0, t;
+    scanf("%d", &n);
+    for(i=0;i<n;i++) {
+        scanf("%d", &a[i]);
+    }
+    for(i=n-1;i>=0;i--) {
+        t=dp(i);
+        if(!max||t>max) max=t;
+    }
+    printf("%d", n-max);
+}
