8b8b242b481d41cf7ab549c367e5b9f7ce0a2769
dnjeh(Notebook) <ehdnj.001@gmail.com>
2023-08-24 08:37:17 +0900

    Today's un-completed baekjoon's

--- _17135.c
+++ _17135.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int vis[15][15], q[1000][2], ind;
-int a[15][15], temp[3][2], n, m, d, now[3];
+int a[15][15], b[15][15], temp[3][2], n, m, d, now[3];
+void set();
 void qput(int y, int x) {
     q[ind][0]=y;
     q[ind++][1]=x;
@@ -10,17 +11,20 @@ int chk(int y, int x, int dy) {
     else return 0;
 }
 int bfs(int dy, int dx) {
-    int i, j, x, y; ind=0;
+    int i, j, x, y, f=0; ind=0;
     for(i=0;i<n;i++) for(j=0;j<m;j++) vis[i][j]=0;
     qput(0, 0);
     for(i=0;i<ind;i++) {
         y=q[i][0], x=q[i][1];
-        if(a[y+dy][x+dx]) break;
+        if(a[y+dy][x+dx]&&!b[y+dy][x+dx]) {
+            f=1;
+            break;
+        }
         if(chk(y, x-1+dx, dy)) { qput(y, x-1); vis[y+dy][x+dx]=1; }
         if(chk(y+1, x+dx, dy)) { qput(y+1, x); vis[y+dy][x+dx]=1; }
         if(chk(y, x+1+dx, dy)) { qput(y, x+1); vis[y+dy][x+dx]=1; }
     }
-    return y*100+x;
+    return f?((y+dy)*100+x+dx):-1;
 }
 int deff() {
     int i, j, ret=0, t;
@@ -29,9 +33,17 @@ int deff() {
             t=bfs(i, now[j]); //무슨 적을 쏴야할 지!
             temp[j][0]=t/100;   
             temp[j][1]=t%100;   
+            printf("%d %d\n", temp[j][0], temp[j][1]);
         }
-
+        for(j=0;j<3;j++) {
+            if(temp[j][1]!=-1&&!b[temp[j][0]][temp[j][1]]) {
+                b[temp[j][0]][temp[j][1]]=1;
+                ret++;
+            }
+        }
     }
+    printf("\n");
+    return ret;
 }
 int main() {
     int i, j, k, t, max;
@@ -47,10 +59,17 @@ int main() {
             now[1]=j;
             for(k=j+1;k<n;k++) {
                 now[2]=k; //유사 백트래킹. 
+                set();
                 t=deff(); //시뮬레이션 결과를 t에 저장한다.
                 if(k==2||t>max) max=t; 
             }
         }
     }
     printf("%d", max); 
+}
+void set() {
+    int i, j;
+    for(i=0;i<n;i++) {
+        for(j=0;j<m;j++) b[i][j]=0;
+    }
 }
