0b7fb5d04d20c90539949b13127006379920a3ec
dnjeh <ehdnj.001@gmail.com>
2023-09-04 22:15:26 +0900

    Today's seen-deux-baekjoon's

--- _1865.c
+++ _1865.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
-int T, n, m, w, a[500][500];
-int aput(int t, int tt, int ttt) {
-
+#define INF 2147483647
+int T, n, m, w, a[500][500], d[500];
+void aput(int t, int tt, int ttt) {
+    if(a[t][tt]>ttt) a[t][tt]=ttt;
+}
+int bf() {
+    int i, j;
+    d[0]=0;
+    for(i=0;i<n-1;i++) {
+        for(j=0;j<n;j++) {
+            if(a[0][j]!=INF&&d[0]+a[0][j]<d[j]) {
+                d[j]=d[0]+a[0][j];
+            }
+        }
+    }
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++) {
+            if(a[i][j]!=INF&&d[i]!=INF&&d[i]+a[i][j]<d[j]) {
+                return 1; 
+            }
+        }
+    }
+    return 0; 
 }
 void set();
 int main() {
     int k, i, j, t, tt, ttt;
     scanf("%d", &T);
     for(k=0;k<T;k++) {
-        set();
         scanf("%d %d %d", &n, &m, &w);
+        set();
         for(i=0;i<m;i++) {
             scanf("%d %d %d", &t, &tt, &ttt);
             t--; tt--;
@@ -21,8 +41,14 @@ int main() {
             t--; tt--;
             aput(t, tt, -ttt); 
         }
+        if(bf()) printf("YES\n");
+        else printf("NO\n");
     }
 }
 void set() {
-    
+    int i, j;
+    for(i=0;i<n;i++) {
+        d[i]=INF;
+        for(j=0;j<n;j++) a[i][j]=INF;
+    }
 }
