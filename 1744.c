a3d42a000345a30e7e8f7f9285c9bc8a680f7686
BuildTools <unconfigured@null.spigotmc.org>
2023-10-01 22:33:42 +0900

    Today's fimallly-completed(greddy) baekjoon's

--- 1744.c
+++ 1744.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define REM -3001
 int a[50], n;
 void merge(int arr[], int left, int middle, int right) {
     int i, j, k;
@@ -23,59 +24,40 @@ void mergeSort(int arr[], int left, int right) {
     }
 }
 int main() {   
-    int i, j, max[3]={0, }, ans=0, left;
+    int i, j, max[3]={0, }, ans=0, left, up, dwn;
     scanf("%d", &n);
     for(i=0;i<n;i++) {
         scanf("%d", &a[i]);
     }
     mergeSort(a, 0, n-1);
-    for(left=n;left>1;) {
-        max[0]=-3001;
-        for(i=0;i<n-1;i++) {
-            for(j=i+1;j<n;j++) {
-                if(a[i]!=-3001&&a[j]!=-3001&&(a[i]*a[j]>max[0]||(a[i]*a[j]==max[0]&&(a[i]+a[j])))) {
-                    max[0]=a[i]*a[j];
-                    max[1]=i;
-                    max[2]=j;
-                }
-            }
-        }
-        if((max[0]>0&&(a[max[1]]==1||a[max[2]]==1))||max[0]<0||(!max[0]&&!(a[max[1]]+a[max[2]]))) {
-            ans+=a[max[1]];
-            ans+=a[max[2]];
-            a[max[1]]=-3001;
-            a[max[2]]=-3001;
+    for(left=n, up=n-1, dwn=0;left>1&&up>=dwn;) {
+        max[0]=REM;
+        if(a[up]*a[up-1]>0&&a[up]*a[up-1]>=a[dwn]*a[dwn+1]&&a[up]!=a[up]*a[up-1]) {
+            ans+=a[up]*a[up-1];
+            a[up]=REM;
+            a[up-1]=REM;
+            up-=2;
             left-=2;
         }
-        else if(max[0]>0||(!max[0]&&a[max[1]]+a[max[2]]<0)) {
-            ans+=max[0];
-            a[max[1]]=-3001;
-            a[max[2]]=-3001;
+        else if(a[up]==1) {
+            ans+=a[up];
+            a[up--]=REM;
+            left--;
+        }
+        else if(a[dwn]*a[dwn+1]>=0&&a[dwn+1]<=0) {
+            ans+=a[dwn]*a[dwn+1];
+            a[dwn]=REM;
+            a[dwn+1]=REM;
+            dwn+=2;
             left-=2;
         }
-        else if(a[max[1]]+a[max[2]]>0) {
-            if(a[max[1]]) {
-                ans+=a[max[1]];
-                a[max[1]]=-3001;
-                left-=1;
-            }
-            else {
-                ans+=a[max[2]];
-                a[max[2]]=-3001;
-                left-=1;
-            }
-        }
-        else if(a[max[1]]+a[max[2]]<0) {
-            if(a[max[1]]) {
-                ans+=a[max[1]];
-                a[max[1]]=-3001;
-                left-=1;
-            }
-            else {
-                ans+=a[max[2]];
-                a[max[2]]=-3001;
-                left-=1;
-            }
+        else {
+            ans+=a[up];
+            ans+=a[up-1];
+            a[up]=REM;
+            a[up-1]=REM;
+            up-=2;
+            left-=2;
         }
     }
     for(i=0;i<n;i++) {
