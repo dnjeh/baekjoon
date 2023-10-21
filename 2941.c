c4751d0e05e1267cc19a21ee3f187f03c50270d9
dnjeh <ehdnj.001@gmail.com>
2023-10-21 23:57:21 +0900

    Today's emergency bawkjoon's

--- 2941.c
+++ 2941.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 int main() {
-    int i, sum=0;
+    int i, sum=0, f;
     char a[101], t, tt;
     scanf("%s", a);
     for(i=0;a[i];i++) {
+        f=0;
         sum++;
+        if(i>=2) {
+            if(t=='d'&&tt=='z'&&a[i]=='=') {
+                sum-=2;
+                f=1;
+            }
+        } 
         if(i) {
             if((tt=='c'&&(a[i]=='='||a[i]=='-')) ||
                (tt=='d'&&a[i]=='-') ||
                (tt=='l'&&a[i]=='j') ||
                (tt=='n'&&a[i]=='j') ||
-               (a[i]=='='&&(tt=='s'||tt=='z'))
+               (tt=='s'&&a[i]=='=') ||
+               (!f&&tt=='z'&&a[i]=='=')
             ) sum--;
-        } 
-        if(i>=2) {
-            if(t=='d'&&tt=='z'&&a[i]=='=') sum-=2;
         } 
         t=tt;
         tt=a[i];
