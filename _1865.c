f2cf05a2d3690b83e56d88ff0d3d1e1ed48b68ff
lilac <109151923+dnjeh@users.noreply.github.com>
2023-10-27 15:11:56 +0900

    Today's seen-un-completed baekjoon's

--- _1865.c
+++ _1865.c
@@ -1,54 +1,66 @@
 #include <stdio.h>
-#define INF 2147483647
-int T, n, m, w, a[500][500], d[500];
-void aput(int t, int tt, int ttt) {
-    if(a[t][tt]>ttt) a[t][tt]=ttt;
+#define INF 2000000000
+long long int d[500], p[3000], vis[500];
+int n, m, w, pind;
+void pput(long long int a, long long int b, long long int c) {
+    p[pind++]=a*100000000+b*100000+c;
 }
 int bf() {
-    int i, j;
-    d[0]=0;
-    for(i=0;i<n-1;i++) {
-        for(j=0;j<n;j++) {
-            if(a[0][j]!=INF&&d[0]+a[0][j]<d[j]) {
-                d[j]=d[0]+a[0][j];
+    long long int i, k, sta, end, cst;
+    for(k=0;k<n-1;k++) {
+        for(i=0;i<pind;i++) {
+            sta=p[i]/100000000; end=p[i]%100000000/100000; cst=p[i]%100000;
+            if(p[i]<0) {
+                sta*=-1;
+                end*=-1;
             }
+            //printf("chk!, %lld %lld %lld\n", sta, end, cst);
+            if(vis[sta]&&d[end]>d[sta]+cst) {
+                //printf("ping!, %lld %lld %lld\n", sta, end, cst);
+                d[end]=d[sta]+cst;
+                vis[end]=1;
+            }  
         }
     }
-    for(i=0;i<n;i++) {
-        for(j=0;j<n;j++) {
-            if(a[i][j]!=INF&&d[i]!=INF&&d[i]+a[i][j]<d[j]) {
-                return 1; 
-            }
-        }
+    for(i=0;i<pind;i++) {
+        sta=p[i]/100000000; end=p[i]%100000000/100000; cst=p[i]%100000;
+        if(vis[sta]&&d[end]>d[sta]+cst) {
+            return 1;
+        }  
     }
-    return 0; 
+    return 0;
 }
 void set();
 int main() {
-    int k, i, j, t, tt, ttt;
+    int i, j, T;
+    long long int t, tt, ttt;
     scanf("%d", &T);
-    for(k=0;k<T;k++) {
+    for(i=0;i<T;i++) {
         scanf("%d %d %d", &n, &m, &w);
         set();
-        for(i=0;i<m;i++) {
-            scanf("%d %d %d", &t, &tt, &ttt);
+        for(j=0;j<m;j++) {
+            scanf("%lld %lld %lld", &t, &tt, &ttt);
             t--; tt--;
-            aput(t, tt, ttt); 
-            aput(tt, t, ttt); 
+            pput(t, tt, ttt);
+            pput(tt, t, ttt);
         }
-        for(i=0;i<w;i++) {
-            scanf("%d %d %d", &t, &tt, &ttt);
+        for(j=0;j<w;j++) {
+            scanf("%lld %lld %lld", &t, &tt, &ttt);
             t--; tt--;
-            aput(t, tt, -ttt); 
+            pput(-t, -tt, -ttt);
         }
         if(bf()) printf("YES\n");
         else printf("NO\n");
     }
 }
 void set() {
-    int i, j;
+    int i, j; pind=0;
+    for(i=0;i<m+w;i++) {
+        p[i]=INF;
+    }
     for(i=0;i<n;i++) {
         d[i]=INF;
-        for(j=0;j<n;j++) a[i][j]=INF;
     }
+    d[0]=0;
+    vis[0]=1;
 }
