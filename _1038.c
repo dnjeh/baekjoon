36d309ee47c89a2d4aaa40e6d3f9110ffc31b0be
lilac <109151923+dnjeh@users.noreply.github.com>
2023-11-30 15:48:41 +0900

    Today's fimally-completed's dfsss's baekjoon's

--- _1038.c
+++ _1038.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 int a[11];   
-int cnt=0, n;
-void back(int tcnt, int t, int f) {
+int cnt=0, n, end;
+void printa(int tind) {
+    if(end) return;
     int i;
-    if(f) {
+    for(i=1;i<=tind;i++) {
+        printf("%d", a[i]);
+    } printf("\n");
+}
+void back(int tcnt, int t, int f) {
+    if(end) return;
+    a[tcnt]=t;
+    int i;
+    if(f<=0) {
+        //printa(tcnt);
         cnt++;
-        if(cnt==n) {
-            for(i=1;i)
+        if(cnt-1==n) {
+            printa(tcnt);
+            end=1;
+            return;
         }
     }
     else {
-
-    }
+        for(i=0;i<t&&!end;i++) {
+            back(tcnt+1, i, f-1);
+        }
+    } 
 }
 int main() {
+    int i;
     scanf("%d", &n);
-    if(!n) {
-        printf("%d", 0);
-        return 0;
+    for(i=1;i<=10;i++) {
+        back(0, 10, i);
     }
-    back(0, 10, 0);
+    if(!end) {
+        printf("-1");
+    }
 }
