001bc60c4bbb1861ae4e525ff3e5585c146af3c8
lilac <109151923+dnjeh@users.noreply.github.com>
2023-12-07 16:24:12 +0900

    Today's nalmuk-half-quater(g3)'s baekjoon's

---
+++ 1695.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int a[5001], b[5001];
+int mem[5001][5001], n;
+
+void initb() {
+    int i; 
+    for(i=n;i>=1;i--) {
+        b[n-i+1]=a[i];
+    }
+}
+
+int main() {
+    int i, j;
+    scanf("%d", &n
+    for(i=0;i<n;i++) {
+        scanf("%d", &a[i+1]);
+    }
+    initb();
+	for(i=1;i<=n;i++) {
+		for(j=1;j<=n;j++) {
+			if(a[i]==b[j]) mem[i][j]=mem[i-1][j-1]+1;
+		    else mem[i][j]=mem[i-1][j]>mem[i][j-1]?mem[i-1][j]:mem[i][j-1];
+        }
+    }
+	printf("%d", n-mem[n][n]);
+}
