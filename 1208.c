5924750ae076aa7fdc7d148c8e92616bc07dc2c1
dnjeh <ehdnj.001@gmail.com>
2024-01-01 17:03:04 +0900

    Today's fimally(seen)-completed(c5) baekjoon's

--- 1208.c
+++ 1208.c
@@ -2,7 +2,6 @@
 #define TSIX 2000000
 long long int a[40], l[4000010], r[4000010];
 long long int n, s;
-long long int min(long long int a, long long int b) { return a<b?a:b; }
 int main() {
     long long int i, j, k, tsum, cnt=0;
     scanf("%lld %lld", &n, &s);
@@ -15,14 +14,15 @@ int main() {
         }   
         l[tsum+TSIX]++;
     }
-    for(i=(1<<(n/2));i<(1<<(n));i+=(1<<(n/2))) {
+    for(i=(1<<(n/2));i<((long long int)1<<(n));i+=(1<<(n/2))) {
         for(j=i,k=n-1,tsum=0;j;j>>=1, k--) {
             if(j&1) tsum+=a[k];
         }   
         r[tsum+TSIX]++;
     }
     for(i=0;i<=TSIX*2;i++) {
-        if(TSIX*2+s-i<=TSIX*2&&l[i]&&r[TSIX*2+s-i]) {
+        //printf("%lld %lld\n", i, TSIX*2+s-i);
+        if(TSIX*2+s-i>=0&&TSIX*2+s-i<=TSIX*2&&l[i]&&r[TSIX*2+s-i]) {
             cnt+=l[i]*r[TSIX*2+s-i];
         }
     }
