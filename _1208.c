52be15febf610f78a6d999c99d45faf56b90e2bb
dnjeh <ehdnj.001@gmail.com>
2024-01-01 15:17:27 +0900

    Today's trois-completed baekjoon's

--- _1208.c
+++ _1208.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #define TSIX 2000000
-int a[40], l[4000010], r[4000010];
-int n, s;
-int min(int a, int b) { return a<b?a:b; }
+long long int a[40], l[4000010], r[4000010];
+long long int n, s;
+long long int min(long long int a, long long int b) { return a<b?a:b; }
 int main() {
-    int i, j, k, tsum, cnt=0;
-    scanf("%d %d", &n, &s);
+    long long int i, j, k, tsum, cnt=0;
+    scanf("%lld %lld", &n, &s);
     for(i=0;i<n;i++) {
-        scanf("%d", &a[i]);
+        scanf("%lld", &a[i]);
     }
     for(i=1;i<(1<<(n/2));i++) {
         for(j=i,k=n-1,tsum=0;j;j>>=1, k--) {
@@ -22,9 +22,9 @@ int main() {
         r[tsum+TSIX]++;
     }
     for(i=0;i<=TSIX*2;i++) {
-        if(l[i]&&r[TSIX*2+s-i]) {
+        if(TSIX*2+s-i<=TSIX*2&&l[i]&&r[TSIX*2+s-i]) {
             cnt+=l[i]*r[TSIX*2+s-i];
         }
     }
-    printf("%d", cnt+l[TSIX+s]+r[TSIX+s]);
+    printf("%lld", cnt+l[TSIX+s]+r[TSIX+s]);
 }
