92844b381d5f879bfee0e4692bc840d86ab00e1a
dnjeh <ehdnj.001@gmail.com>
2024-01-13 14:13:27 +0900

    Today's SEEN-half-quater(g2) baekjoon's

---
+++ 2437.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void merge(int arr[], int left, int middle, int right) {
+    int i, j, k;
+    int n1 = middle-left+1;
+    int n2 = right-middle;
+    int L[n1], R[n2];
+    for(i=0;i<n1;i++) L[i]=arr[left+i];
+    for(j=0;j<n2;j++) R[j]=arr[middle+1+j];
+    for(i=0,j=0,k=left;i<n1&&j<n2;k++) {
+        if(L[i]<=R[j]) arr[k]=L[i++];
+        else arr[k]=R[j++];
+    }
+    for(;i<n1;i++,k++) arr[k]=L[i];
+    for(;j<n2;j++,k++) arr[k] = R[j];
+}
+void mergeSort(int arr[], int left, int right) {
+    if(left<right) {
+        int middle = left + (right - left) / 2;
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+        merge(arr, left, middle, right);
+    }
+}
+int main() {
+    int a[1010];
+    int n, i, sum=1;
+    scanf("%d", &n);
+    for(i=0;i<n;i++) {
+        scanf("%d", &a[i]);
+    }
+    mergeSort(a, 0, n-1);
+    for(i=0;i<n;i++) {
+        if(sum<a[i]) break;
+        sum+=a[i];
+    }
+    printf("%d", sum);
+}
