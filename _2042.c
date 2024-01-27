9ab2e7ae27416de9fb2ba28af0c28348b7901449
dnjeh <ehdnj.001@gmail.com>
2024-01-27 12:48:40 +0900

    Today's deux-completed baekjoon's

--- _2042.c
+++ _2042.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 long long int a[1000000], b[2005000];
 long long int initllst(long long int* frm, long long int* to, int sta, int end, int now);
+long long int sumllst(long long int* tre, int sta, int end, int nsta, int nend, int now);
+void updatllst(long long int* tre, int sta, int end, int ival, int val, int now);
 void initllST(long long int* frm, long long int* to, int n);
 int main() {
     int n, m, k, i, j, t, tt, ttt;
@@ -12,10 +14,12 @@ int main() {
     for(i=0;i<m+k;i++) {
         scanf("%d %d %d", &t, &tt, &ttt);
         if(t==1) {
-            updatllst(b, tt, ttt, 1);
+            t=ttt-a[tt-1];
+            a[tt-1]=ttt;
+            updatllst(b, 1, n, tt, t, 1);
         }
         else {
-            printf("%lld\n", sumllst(b, tt, ttt, 1));
+            printf("%lld\n", sumllst(b, tt, ttt, 1, n, 1));
         }
     }
 }
@@ -30,10 +34,18 @@ void initllST(long long int* frm, long long int* to, int n) {
     initllst(frm, to, 1, n, 1);
 }
 
-long long int sumllst(long long int* tre, int sta, int end, int now) {
-
+long long int sumllst(long long int* tre, int sta, int end, int nsta, int nend, int now) {
+    if(sta>nend||end<nsta) return 0;
+    if(sta<=nsta&&end<=nend) return tre[now];
+    int nmid=(nsta+nend)/2;
+    return sumllst(tre, sta, end, nsta, nmid, now*2)+sumllst(tre, sta, end, nmid+1, nend, now*2+1);
 }
 
-long long int updatllst(long long int* tre, int sta, int end, int now) {
-
+void updatllst(long long int* tre, int sta, int end, int ival, int val, int now) {
+    if(ival<sta||ival>end) return;
+    tre[now]+=val;
+    if(sta==end) return;
+    int mid=(sta+end)/2;
+    updatllst(tre, sta, mid, ival, val, now*2);
+    updatllst(tre, mid+1, end, ival, val, now*2+1);
 }
