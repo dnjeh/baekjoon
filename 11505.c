c5ef675f8e4cbdea7b3d501bfcfba41418b69f50
dnjeh <ehdnj.001@gmail.com>
2024-02-07 21:13:09 +0900

    Today's fimally-completed(c6) baekjoon's

--- 11505.c
+++ 11505.c
@@ -3,7 +3,7 @@
 long long int a[1000001], b[2100000];
 long long int initllst(long long int* frm, long long int* to, int sta, int end, int now);
 long long int mulllst(long long int* tre, int sta, int end, int nsta, int nend, int now);
-long long int updatllst(long long int* frm, long long int* tre, int sta, int end, int ival, int now);
+long long int updatllst(long long int* tre, int sta, int end, int ival, int val, int now);
 void initllST(long long int* frm, long long int* to, int n);
 int ind() {
 
@@ -18,8 +18,7 @@ int main() {
     for(i=0;i<m+k;i++) {
         scanf("%lld %lld %lld", &t, &tt, &ttt);
         if(t==1) {
-            b[ind()]=ttt;
-            updatllst(a, b, 1, n, t, 1);
+            updatllst(b, 1, n, tt, ttt, 1);
         }
         else {
             printf("%lld\n", mulllst(b, tt, ttt, 1, n, 1));
@@ -44,9 +43,9 @@ long long int mulllst(long long int* tre, int sta, int end, int nsta, int nend, 
     return mulllst(tre, sta, end, nsta, nmid, now*2)*mulllst(tre, sta, end, nmid+1, nend, now*2+1)%MOD;
 }
 
-long long int updatllst(long long int* frm, long long int* tre, int sta, int end, int ival, int now) {
-    if(ival<sta||ival>end) return 1;
-    if(sta==end) return tre[now];
+long long int updatllst(long long int* tre, int sta, int end, int ival, int val, int now) {
+    if(ival<sta||ival>end) return tre[now];
+    if(sta==end) return tre[now]=val;
     int mid=(sta+end)/2;
-    return tre[now]=updatllst(frm, tre, sta, mid, ival, now*2)*updatllst(frm, tre, mid+1, end, ival, now*2+1)%MOD;
+    return tre[now]=updatllst(tre, sta, mid, ival, val, now*2)*updatllst(tre, mid+1, end, ival, val, now*2+1)%MOD;
 }
