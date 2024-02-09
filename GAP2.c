a81a34ed2a488c5b2d0412d7d4427e1db8c7f9d4
dnjeh <ehdnj.001@gmail.com>
2024-02-09 12:22:40 +0900

    Try it!

---
+++ GAP2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int f(int n) {
+    if (n == 0)
+        return 1;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += f(i);
+    }
+    return sum % 13;
+}
+
+int main() {
+    char ans[] = "rgjgmbuyhbfcx";
+    for (int i = 0; i < 13; i++) {
+        printf("%d\n", i);
+        ans[i] ^= f(13 + i * i * i);
+        puts(ans);
+    }
+    puts(ans);
+}
