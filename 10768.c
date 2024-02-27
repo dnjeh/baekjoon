003752c1d89beeb2663e1d4c39bc8f01fb18a7d0
dnjeh <ehdnj.001@gmail.com>
2024-02-27 14:13:23 +0900

    Today's

---
+++ 10768.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int m, d;
+    scanf("%d %d", &m, &d);
+    if(m<2||(m==2&&d<18)) printf("Before");
+    else if(m==2&&d==18) printf("Special");
+    else printf("After");
+}
