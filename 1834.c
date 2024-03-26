77f8ca682a3d3959d09715108fdbc192cfa4a9c4
lilac <109151923+dnjeh@users.noreply.github.com>
2024-03-26 10:32:30 +0900

    Today's baekjoon's

---
+++ 1834.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    long long int n, sum=0;
+    scanf("%lld", &n);
+    for(int i=1;i<n;i++) {
+        sum+=(n+1)*i;
+    }
+    printf("%lld", sum);
+    return 0;
+}
