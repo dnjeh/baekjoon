4a089de5964124867c1d7ad9146450de89bbc191
dnjeh <ehdnj.001@gmail.com>
2024-07-23 22:30:53 +0900

    Today's baekjoon's

--- 15725.c
+++ 15725.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-int a[200000];
 int main() {
-    long long int n, k, sum=0, max=0;
-    scanf("%lld %lld", &n, &k);
-    for(int i=0;i<n;i++) {
-        scanf("%d", &a[i]);
-        if(!i||max<a[i]-i*k) max=a[i]-i*k; 
+    int a=-10001, f=0, ff=0;
+    char t[20];
+    scanf("%s", t);
+    for(int i=0;t[i];i++) {
+        if(t[i]=='-') f=1;
+        if(t[i]<='9'&&t[i]>='0') {
+            if(a==-10001) a=0;
+            a*=10;
+            a+=t[i]-'0';
+        }
+        if(t[i]=='x') {
+            ff=1;
+            break;
+        }
     }
-    for(int i=0;i<n;i++) {
-        sum+=max+i*k-a[i];
-    }
-    printf("%lld", sum);
+    if(a==-10001) a=1;
+    if(!ff) printf("0");
+    else if(f) printf("%d", a*-1);
+    else printf("%d", a);
 }
