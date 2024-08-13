f7551cc651e7da4e32ba9419ef3a8c63c86aec0d
dnjeh <ehdnj.001@gmail.com>
2024-08-13 18:10:11 +0900

    Today's fimmaly-baekjoon's

--- 6059.c
+++ 6059.c
@@ -1,42 +1,36 @@
 #include <stdio.h>
-int a[1000][1000], vis[1000], mem[1000][1000], n, q, nlnow;
-int dfs(int now, int end) {
-    int ret=0, t;
-    //printf("> %d %d %d\n", now, end, ret);
-    vis[now]=nlnow;
-    if(mem[now][end]!=-1) ret=mem[now][end];
-    else {
-        for(int i=0;i<n;i++) {
-            if(vis[i]!=nlnow&&a[now][i]&&i==end) {
-                ret = a[now][i];
-                break;
-            }
-            if(vis[i]!=nlnow&&a[now][i]&&(t=dfs(i, end))) {
-                ret = t+a[now][i]; 
-                break;
-            }
+int a[1000][1000], vis[1000], n, qq, nlnow, q[1010][2], qind;
+void qput(int t, int tt) {
+    vis[t]=nlnow;
+    q[qind][0]=t;
+    q[qind++][1]=tt;
+}
+int dfs(int sta, int end) {
+    int ind;
+    qput(sta, 0);
+    for(int i=0;;ind=qind) {
+        for(;i<ind;i++) {
+            int now=q[i][0], len=q[i][1];
+            if(now==end) return len;
+            for(int j=0;j<n;j++) 
+                if(vis[j]!=nlnow&&a[now][j]) 
+                    qput(j, len+a[now][j]);
         }
-        mem[now][end]=ret;
     }
-    return ret;
+    return 0;
 }
 int main() {
     int t, tt, ttt;
-    scanf("%d %d", &n, &q);
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
-            mem[i][j]=-1;
-        }
-    }
+    scanf("%d %d", &n, &qq);
     for(int i=0;i<n-1;i++) {
         scanf("%d %d %d", &t, &tt, &ttt);
         t--; tt--;
         a[t][tt]=ttt;
         a[tt][t]=ttt;
     }
-    for(nlnow=1;nlnow<=q;nlnow++) {
+    for(nlnow=1;nlnow<=qq;nlnow++) {
         scanf("%d %d", &t, &tt);
-        t--; tt--;
+        qind=0; t--; tt--;
         printf("%d\n", dfs(t, tt));
     }
     return 0;
