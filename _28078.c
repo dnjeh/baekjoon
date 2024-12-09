ad7d04e07b415edc7d8db33a06289cd1a8a1aca7
lilac <109151923+dnjeh@users.noreply.github.com>
2024-12-09 16:54:00 +0900

    Today's un baek

---
+++ _28078.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#define SIZ 500100
+int a[SIZ], sta, end, siz, bcnt, wcnd, dir; 
+int main() {
+    char t[10], tt;
+    int q;
+    scanf("%d", &q);
+    for(int i=0;i<q;i++) {
+        scanf(" %s", t);
+        if(strcmp(t, "pop")) {
+            scanf(" %c", &tt);
+        }
+        else {
+            int it=pop();
+            if(!it&&dir==)
+        }
+    }
+    return 0;
+}
