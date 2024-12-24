8b6925c53e07579e0bdcdb09481209ac65f5ad59
dnjeh <ehdnj.001@gmail.com>
2024-12-24 20:24:40 +0900

    Today's half baekjoon's

--- _3830.c
+++ _3830.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
-long long int a[2][100010], q[2][100100], n, m;
+int a[2][100010], q[100100], n, m;
 void set();
-long long int find(long long int t) {
-    long long int ind=1, i, ret, ret2=0, sum;
-    q[0][0]=t;
+int find(int t, int f) {
+    int ind=1, i, ret, ret2=0;
+    q[0]=t;
     for(i=0;i<ind;i++) {
         if(q[i]!=a[0][q[i]]) {
             q[ind++]=a[0][q[i]];
         }
     }
     ret=q[i-1];
-    for(i=0;i<ind;i++) a[q[i]]=ret;
-    return ret2*1000000+ret;
+    for(i=ind-2;i>=0;i--) {
+        a[0][q[i]]=ret;
+        a[1][q[i]]+=a[1][q[i+1]];
+    }
+    ret2=a[1][t];
+    if(f) return ret2;
+    else return ret;
 }
 int main() {
-    long long int i, j, t1, t2, t3, t4, t5;
+    int i, j, t1, t2, t3, t4, t5;
     char mod;
-    for(scanf("%lld %lld", &n, &m);!n||!m;scanf("%lld %lld", &n, &m);) {
+    for(scanf("%d %d", &n, &m);n||m;scanf("%d %d", &n, &m)) {
         set();   
         for(i=0;i<m;i++) {
-            scanf(" %c %lld %lld", &mod, &t1, &t2);
+            scanf(" %c %d %d", &mod, &t1, &t2);
             if(mod=='!') {
-                scanf("%lld", &t5);
-                t1=find(t1); t2=find(t2);
-                t3=t1/1000000; t1%=1000000;
-                t4=t2/1000000; t2%=1000000;
+                scanf("%d", &t5);
+                t3=find(t1, 1); t4=find(t2, 1);
+                t1=find(t1, 0); t2=find(t2, 0);
                 if(t1>t2) {
                     a[0][t1]=t2;
-                    a[1][t1]=t5+t3;
+                    a[1][t1]=t5+t4-t3;
                 }
                 else {
                     a[0][t2]=t1;
-                    a[1][t2]=t4-t5
+                    a[1][t2]=t3-t5-t4;
                 }
             }
             else {
-                t1=find(t1); t2=find(t2);
-                t3=t1/1000000-t2/1000000;
-                t1%=1000000; t2%=1000000;
-                if(t1!=t2) printf("%lld\n", t3);
+                t3=find(t1, 1)-find(t2, 1);
+                t1=find(t1, 0); t2=find(t2, 0);
+                if(t1==t2) printf("%d\n", t3);
                 else printf("UNKNOWN\n");
             }
+            //for(int j=0;j<2;j++) {
+            //    for(int i=1;i<=n;i++) {
+            //        printf("%3d ", a[j][i]);
+            //    }   
+            //    printf("\n");
+            //}
+            //printf("-----------\n");
         }
     }
 }
