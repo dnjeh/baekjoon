97bc68b88afc6a482c64f3d147af0182a6b1c08d
dnjeh <ehdnj.001@gmail.com>
2025-06-13 21:29:48 +0900

    Today's baekjoon's

---
+++ 20232.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char *ans;
+
+    switch (n)
+    {
+    case 1995:
+        ans = "ITMO";
+        break;
+    case 1996:
+        ans = "SPbSU";
+        break;
+    case 1997:
+        ans = "SPbSU";
+        break;
+    case 1998:
+        ans = "ITMO";
+        break;
+    case 1999:
+        ans = "ITMO";
+        break;
+    case 2000:
+        ans = "SPbSU";
+        break;
+    case 2001:
+        ans = "ITMO";
+        break;
+    case 2002:
+        ans = "ITMO";
+        break;
+    case 2003:
+        ans = "ITMO";
+        break;
+    case 2004:
+        ans = "ITMO";
+        break;
+    case 2005:
+        ans = "ITMO";
+        break;
+    case 2006:
+        ans = "PetrSU, ITMO";
+        break;
+    case 2007:
+        ans = "SPbSU";
+        break;
+    case 2008:
+        ans = "SPbSU";
+        break;
+    case 2009:
+        ans = "ITMO";
+        break;
+    case 2010:
+        ans = "ITMO";
+        break;
+    case 2011:
+        ans = "ITMO";
+        break;
+    case 2012:
+        ans = "ITMO";
+        break;
+    case 2013:
+        ans = "SPbSU";
+        break;
+    case 2014:
+        ans = "ITMO";
+        break;
+    case 2015:
+        ans = "ITMO";
+        break;
+    case 2016:
+        ans = "ITMO";
+        break;
+    case 2017:
+        ans = "ITMO";
+        break;
+    case 2018:
+        ans = "SPbSU";
+        break;
+    case 2019:
+        ans = "ITMO";
+        break;
+
+    default:
+        break;
+    }
+    printf("%s", ans);
+}
