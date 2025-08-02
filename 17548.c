604e2d9b950fdad987917eadab51752ce65b9395
dnjeh <ehdnj.001@gmail.com>
2025-08-02 21:34:52 +0900

    Today's baekjoon's

---
+++ 17548.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int cnt=0;
+    char a[1010];
+    scanf("%s", a);
+    cnt=(strlen(a)-2)*2;
+    printf("h");
+    for(int i=0;i<cnt;i++) {
+        printf("e");
+    }
+    printf("y");
+}
