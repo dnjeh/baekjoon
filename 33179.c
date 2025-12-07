f1958ec6838396d7343f6ac7a04fe2787641334f
dnjeh <ehdnj.001@gmail.com>
2025-12-07 18:14:40 +0900

    Today's baekjoon's

---
+++ 33179.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n, t, ans=0;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++) {
+        scanf("%d", &t);
+        ans+=t/2+t%2;
+    }
+    printf("%d", ans);
+}
