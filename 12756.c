95ed360c167e5531acdce1181086e4d02e9d02e3
dnjeh <ehdnj.001@gmail.com>
2026-04-20 20:36:08 +0900

    Today's baekjoon's

---
+++ 12756.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    for(;b<=0||d<=0;) {
+        b-=c;
+        d-=a;
+    }
+    if(b<=0&&d<=0) printf("DRAW");
+    else if(b<=0) printf("PLAYER B");
+    else printf("PLAYER A");
+}
